19d1f5c2c907cbff226c24b770423c7e0eb2a9aa
Eldar Djafarov <djkojb@gmail.com>
2019-04-08 16:29:10 +0200

    fix

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "null_data_source" "tags_as_list_of_maps" {
   count = "${length(keys(var.tags))}"
 
-  inputs = "${merge(map(
+  inputs = "${concat(map(
     "key", "${element(keys(var.tags), count.index)}",
     "value", "${element(values(var.tags), count.index)}",
     "propagate_at_launch", true
